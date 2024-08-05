0eb00bac9eba6e1b2b6c5ae412549ce7e79730cb
InactiveGaming13 <116043268+InactiveGaming13@users.noreply.github.com>
2024-08-05 22:07:35 +1000

    Convert main.py to use the newer RPC system and use config.json.

--- main/main.py
+++ main/main.py
@@ -6,36 +6,102 @@ This file should be run on the client where the Plex Media Server and Discord ar
 from flask import Flask, request
 from json import loads
 from pypresence import Presence
+from requests import get
+from threading import Thread
+from time import sleep
+
+# Get the config params.
+print("Reading config file")
+with open("remoteConfig.json", "r") as file:
+    config: dict[str, str] = loads(file.read())
+    print("Config file read successfully")
 
 app: Flask = Flask(__name__)
 
-discordClientId: str = "YOUR_DISCORD_CLIENT_ID"
+RPC: Presence = Presence(config["discordClientId"])
 
-RPC: Presence = Presence(discordClientId)
+# Declare the currentlyPlaying and lastPlayed dictionaries.
+currentlyPlaying: dict[str, dict[str, str | int]] = {}
+lastPlayed: currentlyPlaying = {}
 
-currentlyPlaying: dict[str, str] = {}
+
+def clearRPC(delay: float) -> None:
+    """
+    This function clears the Discord RPC status.
+    """
+
+    # Clear the Discord RPC status after the delay if nothing is being played on the PMS.
+    lastPlayed.clear()
+    lastPlayed.update(currentlyPlaying)
+    currentlyPlaying.clear()
+    sleep(delay)
+    if len(currentlyPlaying) == 0:
+        RPC.clear()
+        print("Cleared Discord RPC status due to the queue being empty.")
+        print("If the queue is not empty (This is a bug and should be reported at https://github.com/InactiveGaming13/Plex-RPC/issues), the status will be updated shortly.")
 
 
-def updatePresence(metadataTitle: str, metadataArtists: str, serverName: str, playing: bool = True) -> None:
+def updatePresence(data: dict[str, str] | None, playing: bool = True) -> None:
     """
     This function updates the Discord RPC status with the provided parameters.
 
     Args:
-        metadataTitle (str): The title of the media being played.
-        metadataArtists (str): The artist of the media being played.
-        serverName (str): The name of the server on which the media is being played.
+        data (dict[str, str]): The data sent from the server.
         playing (bool, optional): Whether the media is playing or not. Defaults to True.
     """
-    if playing:
+    # Declare albumImage as None so that it always exists.
+    albumImage: str | None = None
+
+    # Check if Last.fm is enabled and if the media is playing and if the data is not None.
+    if config["lastFmEnabled"] and playing and data:
+        # Convert the metadataArtists string to a list and get the first artist.
+        artist: str = data["metadataArtists"].split(";")[0]
+        # Create the Last.fm request URL.
+        lastFmRequest: str = f"https://ws.audioscrobbler.com/2.0/?method=album.getInfo&api_key={config["lastFmApiKey"]}&artist={artist}&album={data["albumName"]}&format=json"
+        # Get the Last.fm response.
+        lastFmResponse: dict[str, str] = get(lastFmRequest).json()
+        # Get the album image from the Last.fm response.
+        albumImage = lastFmResponse["album"]["image"][3]["#text"] if "album" in lastFmResponse else "plex-icon"
+
+    # Check if the media is playing and if the data is not None.
+    if playing and data:
+        # Replace the semicolon with a comma and space in the metadataArtists string.
+        data["metadataArtists"] = data["metadataArtists"].replace(";", ", ")
+
+        # Check if the currentlyPlaying dictionary is not empty and clear it.
+        if len(currentlyPlaying) > 0:
+            currentlyPlaying.clear()
+
+        # Get the last used PID.
+        lastUsed: int = 0 if len(lastPlayed) == 0 else 1 if next(iter(lastPlayed.values()))["pid"] == 0 else 0
+
+        # Update the currentlyPlaying dictionary with the data and last used PID.
+        currentlyPlaying[data["metadataTitle"]] = {"artist": data["metadataArtists"], "pid": lastUsed}
+
+        # Clear the Discord RPC status so that the new status can be set and there is only one status.
+        RPC.clear(next(iter(lastPlayed.values()))["pid"]) if len(lastPlayed) > 0 else None
+
+        # Update the Discord RPC status with the data.
         RPC.update(
-            state=f"by {metadataArtists}",
-            details=metadataTitle,
-            large_image="plex-icon",
-            large_text=f"Listening on {serverName}",
+            details=data["metadataTitle"],
+            state=f"by {data["metadataArtists"]}",
+            large_image=albumImage,
+            large_text=f"{data["albumName"]}",
+            small_image="plex-icon" if albumImage != "plex-icon" else None,
+            small_text=f"Listening on {data["serverName"]}",
             type=2
         )
-    else:
-        RPC.clear()
+        return
+
+    # This will only happen if the media is not playing.
+    match data["eventType"]:
+        # If the media is paused, clear the Discord RPC status.
+        case "media.pause":
+            RPC.clear()
+
+        # If the media is stopped, clear the Discord RPC status if nothing is playing after 5 seconds..
+        case "media.stop":
+            Thread(target=clearRPC, args=(5,)).start()
 
 
 @app.post("/")
@@ -56,33 +122,33 @@ def index() -> str:
     metadataTitle: str = data["Metadata"]["title"]
     # Below is required as the "originalTitle" key is only present when the grandparentTitle is different to the artist's name.
     metadataArtists: str = data["Metadata"]["originalTitle"] if "originalTitle" in data["Metadata"] else data["Metadata"]["grandparentTitle"]
-    # albumName: str = data["Metadata"]["parentTitle"]
+    albumName: str = data["Metadata"]["parentTitle"]
+
+    data: dict[str, str] = {
+        "metadataTitle": metadataTitle,
+        "metadataArtists": metadataArtists,
+        "albumName": albumName,
+        "serverName": serverName
+    }
 
     match eventType:
         case "media.play":
-            print("Playing")
-            currentlyPlaying[metadataTitle] = metadataArtists
-            updatePresence(metadataTitle, metadataArtists, serverName)
+            updatePresence(data)
 
         case "media.resume":
-
-            print("Resuming")
-            updatePresence(metadataTitle, metadataArtists, serverName)
+            updatePresence(data)
 
         case "media.pause":
-            print("Paused")
-            updatePresence(metadataTitle, metadataArtists, serverName, False)
+            updatePresence({"eventType": "media.pause"}, False)
 
         case "media.stop":
-            print("Stopped")
-            updatePresence(metadataTitle, metadataArtists, serverName, False)
+            updatePresence({"eventType": "media.stop"}, False)
 
         case "media.scrobble":
-            print("Scrobbled")
-            updatePresence(metadataTitle, metadataArtists, serverName)
+            pass
 
         case _:
-            print("Unknown Event")
+            print(f"Unknown Event -> {eventType}")
             pass
 
     return "OK"
