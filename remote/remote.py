848b9fe131cfa4e5d9d3e411a86bc4012840bbb5
InactiveGaming13 <116043268+InactiveGaming13@users.noreply.github.com>
2024-08-05 21:49:20 +1000

    Drastically reduce the delay between switching the RPC data.

--- remote/remote.py
+++ remote/remote.py
@@ -2,16 +2,36 @@ from json import loads
 from pypresence import Presence
 from socketio import Client
 from requests import get
+from time import sleep
+from threading import Thread
 
 print("Reading config file")
 with open("remoteConfig.json", "r") as file:
     config: dict[str, str] = loads(file.read())
+    print("Config file read successfully")
 
 RPC: Presence = Presence(config["discordClientId"])
 
 socketio: Client = Client()
 
-currentlyPlaying: dict[str, str] = {}
+currentlyPlaying: dict[str, dict[str, str | int]] = {}
+lastPlayed: currentlyPlaying = {}
+
+
+def clearRPC(delay: float) -> None:
+    """
+    This function clears the Discord RPC status.
+    """
+
+    #
+    lastPlayed.clear()
+    lastPlayed.update(currentlyPlaying)
+    currentlyPlaying.clear()
+    sleep(delay)
+    if len(currentlyPlaying) == 0:
+        RPC.clear()
+        print("Cleared Discord RPC status due to the queue being empty.")
+        print("If the queue is not empty (This is a bug and should be reported at https://github.com/InactiveGaming13/Plex-RPC/issues), the status will be updated shortly.")
 
 
 def updatePresence(data: dict[str, str] | None, playing: bool = True) -> None:
@@ -22,34 +42,59 @@ def updatePresence(data: dict[str, str] | None, playing: bool = True) -> None:
         data (dict[str, str]): The data sent from the server.
         playing (bool, optional): Whether the media is playing or not. Defaults to True.
     """
-
+    # Declare albumImage as None so that it always exists.
     albumImage: str | None = None
 
+    # Check if Last.fm is enabled and if the media is playing and if the data is not None.
     if config["lastFmEnabled"] and playing and data:
-        lastFmRequest: str = f"https://ws.audioscrobbler.com/2.0/?method=album.getInfo&api_key={config["lastFmApiKey"]}&artist={data["metadataArtists"]}&album={data["albumName"]}&format=json"
+        # Convert the metadataArtists string to a list and get the first artist.
+        artist: str = data["metadataArtists"].split(";")[0]
+        # Create the Last.fm request URL.
+        lastFmRequest: str = f"https://ws.audioscrobbler.com/2.0/?method=album.getInfo&api_key={config["lastFmApiKey"]}&artist={artist}&album={data["albumName"]}&format=json"
+        # Get the Last.fm response.
         lastFmResponse: dict[str, str] = get(lastFmRequest).json()
+        # Get the album image from the Last.fm response.
+        albumImage = lastFmResponse["album"]["image"][3]["#text"] if "album" in lastFmResponse else "plex-icon"
 
-        if "album" in lastFmResponse:
-            albumImage = lastFmResponse["album"]["image"][3]["#text"]
-
+    # Check if the media is playing and if the data is not None.
     if playing and data:
-        if len(currentlyPlaying) > 1:
+        # Replace the semicolon with a comma and space in the metadataArtists string.
+        data["metadataArtists"] = data["metadataArtists"].replace(";", ", ")
+
+        # Check if the currentlyPlaying dictionary is not empty and clear it.
+        if len(currentlyPlaying) > 0:
             currentlyPlaying.clear()
-        currentlyPlaying[data["metadataTitle"]] = data["metadataArtists"]
-        if not RPC.pipe:
-            RPC.connect()
+
+        # Get the last used PID.
+        lastUsed: int = 0 if len(lastPlayed) == 0 else 1 if next(iter(lastPlayed.values()))["pid"] == 0 else 0
+
+        # Update the currentlyPlaying dictionary with the data and last used PID.
+        currentlyPlaying[data["metadataTitle"]] = {"artist": data["metadataArtists"], "pid": lastUsed}
+
+        # Clear the Discord RPC status so that the new status can be set and there is only one status.
+        RPC.clear(next(iter(lastPlayed.values()))["pid"]) if len(lastPlayed) > 0 else None
+
+        # Update the Discord RPC status with the data.
         RPC.update(
             details=data["metadataTitle"],
             state=f"by {data["metadataArtists"]}",
-            large_image=albumImage if albumImage else "plex-icon",
+            large_image=albumImage,
             large_text=f"{data["albumName"]}",
-            small_image="plex-icon" if albumImage else None,
-            small_text=f"Listening on {data["serverName"]}" if albumImage else None,
+            small_image="plex-icon" if albumImage != "plex-icon" else None,
+            small_text=f"Listening on {data["serverName"]}",
             type=2
         )
-    else:
-        currentlyPlaying.clear()
-        RPC.clear()
+        return
+
+    # This will only happen if the media is not playing.
+    match data["eventType"]:
+        # If the media is paused, clear the Discord RPC status.
+        case "media.pause":
+            RPC.clear()
+
+        # If the media is stopped, clear the Discord RPC status if nothing is playing after 5 seconds..
+        case "media.stop":
+            Thread(target=clearRPC, args=(5,)).start()
 
 
 @socketio.on("connect")
@@ -77,7 +122,6 @@ def play(data: dict[str, str]) -> None:
     Args:
         data (dict[str, str]): The data sent from the server.
     """
-    print("Playing")
     updatePresence(data)
 
 
@@ -89,7 +133,6 @@ def resume(data: dict[str, str]) -> None:
     Args:
         data (dict[str, str]): The data sent from the server.
     """
-    print("Resuming")
     updatePresence(data)
 
 
@@ -98,8 +141,7 @@ def pause() -> None:
     """
     This function is called when the server sends a "pause" event to the client.
     """
-    print("Paused")
-    updatePresence(None, False)
+    updatePresence({"eventType": "media.pause"}, False)
 
 
 @socketio.on("stop")
@@ -107,8 +149,7 @@ def stop() -> None:
     """
     This function is called when the server sends a "stop" event to the client.
     """
-    print("Stopped")
-    updatePresence(None, False)
+    updatePresence({"eventType": "media.stop"}, False)
 
 
 if __name__ == "__main__":
@@ -116,7 +157,7 @@ if __name__ == "__main__":
     This is the main function that connects to the server and Discord RPC.
     """
     try:
-        print("Connecting to DiscordRPC server")
+        print("Connecting to DiscordRPC")
         RPC.connect()
         ip: str = f"{config["serverProtocol"]}://{config["serverIp"]}:{config["serverPort"]}"
         print(f"Connecting to Socket server -> {ip}")
